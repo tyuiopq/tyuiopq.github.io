ae581a972e2b98f936cda39c168ce49ae27c2b6a
tyuiopq <purplekvass@yahoo.com>
2026-07-04 19:35:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'habits-v2';
+const FILES = ['.', 'index.html', 'manifest.json', 'icon-180.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return r;
+      })
+      .catch(() => caches.match(e.request, { ignoreSearch: true }))
+  );
+});
